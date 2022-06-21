b83d7ccb3114257920b406398b4be51aceaf0225
Yomna602 <63078572+Yomna602@users.noreply.github.com>
2022-06-21 16:23:38 +0300

    server: Real-time Collaboration

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const inOut = new Server(PORT,{
 inOut.on('connection', socket => {
     
     socket.on('send-changes', delta => {
-        console.log('connected with changes ', delta );
-
+        console.log(delta);
+        socket.broadcast.emit('receive-changes', delta);
     })
 });
