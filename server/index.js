ec1d59055fd24f13e78665b54ec0337849475486
salma-ehab <salma.ehabg@gmail.com>
2022-06-21 11:25:14 +0200

    Server: Socket

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import {Server} from 'socket.io'
+
+const PORT = 9000;
+
+const inOut = new Server(PORT, {
+    cors:{
+        origin: 'http://localhost:3000/',
+        methods: ['GET', 'POST']
+    }
+});
+
+inOut.on('connection', socket => {})
