4750c1f494b7425eb4cbe6ed3b80b7f6750dd4eb
sama <sama2ayman2@gmail.com>
2022-06-21 13:58:15 +0200

    dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 import Docs from './item/Docs';
 function App() {
   return (
-    <>
-      <Docs />
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Docs/>} />
+      </Routes>
+    </Router>
   );
 }
 
