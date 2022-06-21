4346f2250ce1a4ee7f6b90c876f873699d800ab5
salmastudy1 <63376883+salmastudy1@users.noreply.github.com>
2022-06-21 16:14:27 +0200

    Client: Uid

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
+import {v4 as uuid} from 'uuid';
 
 import Docs from './item/Docs';
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Docs/>} />
+        <Route path='/' element={<Navigate replace to = {`/docs/${uuid()}`} />} />
+        <Route path='/docs/:id' element={<Docs/>} />
       </Routes>
     </Router>
   );
